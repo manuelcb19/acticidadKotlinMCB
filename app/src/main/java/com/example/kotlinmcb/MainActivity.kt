82b00a02c1a18905be2bdb7d55c8f1b289628f2b
mcbtr <mcb.gitlab@gmail.com>
2024-02-22 12:55:04 +0100

    Se consigue mandar un mensaje por parametro desde un text editable.

--- app/src/main/java/com/example/kotlinmcb/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmcb/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         val bodyLayout = findViewById<FrameLayout>(R.id.bodyLayout)
         val btnChangeColor = findViewById<Button>(R.id.btnChangeColor)
         val btnSwitchActivity = findViewById<Button>(R.id.btnSwitchActivity)
+        val editTextMessage = findViewById<EditText>(R.id.editTextMessage)
 
         btnChangeColor.setOnClickListener {
             val newColor = Color.rgb((0..255).random(), (0..255).random(), (0..255).random())
@@ -23,7 +25,12 @@ class MainActivity : AppCompatActivity() {
         }
 
         btnSwitchActivity.setOnClickListener {
+
+            val message = editTextMessage.text.toString()
+
             val intent = Intent(this, MainActivity_2::class.java)
+            intent.putExtra("EXTRA_MESSAGE", message)
+
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/kotlinmcb/MainActivity_2.kt
+++ app/src/main/java/com/example/kotlinmcb/MainActivity_2.kt
@@ -2,10 +2,17 @@ package com.example.kotlinmcb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity_2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_2)
+
+
+        val message = intent.getStringExtra("EXTRA_MESSAGE")
+
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "Bienvenido a la App\n$message"
     }
 }
