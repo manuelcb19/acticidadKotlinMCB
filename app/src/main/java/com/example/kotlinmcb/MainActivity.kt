2a8cc27f2c6f6c2c40d54555aa8aa196926adb6c
mcbtr <mcb.gitlab@gmail.com>
2024-02-22 12:16:00 +0100

    Actividad 4. Se implementan dos botones, uno cambia el color con un ramdon, por lo tanto siempore pone un color distinto y el otro boton cambia de actividad

--- app/src/main/java/com/example/kotlinmcb/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmcb/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.kotlinmcb
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bodyLayout = findViewById<FrameLayout>(R.id.bodyLayout)
+        val btnChangeColor = findViewById<Button>(R.id.btnChangeColor)
+        val btnSwitchActivity = findViewById<Button>(R.id.btnSwitchActivity)
+
+        btnChangeColor.setOnClickListener {
+            val newColor = Color.rgb((0..255).random(), (0..255).random(), (0..255).random())
+            bodyLayout.setBackgroundColor(newColor)
+        }
+
+        btnSwitchActivity.setOnClickListener {
+            val intent = Intent(this, MainActivity_2::class.java)
+            startActivity(intent)
+        }
     }
 }
