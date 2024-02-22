8af7a1e1d92882f56431f37fb33bf3ecc6391ee1
mcbtr <mcb.gitlab@gmail.com>
2024-02-22 18:10:04 +0100

    Se crea la clase auth para gestionar la autentificacion, ahora se creara el login y el register

---
+++ app/src/main/java/com/example/kotlinmcb/AuthActivity.kt
@@ -0,0 +1,41 @@
+package com.example.kotlinmcb
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class AuthActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+        auth = FirebaseAuth.getInstance()
+
+        if (auth.currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, LoginFragment())
+                .commit()
+        }
+    }
+
+    fun switchToRegistroFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, RegistroFragment())
+            .addToBackStack(null)
+            .commit()
+    }
+
+    fun onLoginSuccess() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
